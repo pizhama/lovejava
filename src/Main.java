2febf3e8d8f149fdc2a4437570111f02f74069f9
pizhama <pizhama123@gmail.com>
2024-01-20 05:41:27 +0600

    oop

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Book deadSouls = new Book("Dead Souls", "Nikolai Gogol", 352);
+        deadSouls.displayInfo();
 
+        deadSouls.setTitle("Set123");
+
+        System.out.println(deadSouls.getTitle());
+
+        
     }
 }
